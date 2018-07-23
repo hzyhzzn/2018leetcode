a8bc5985e4d715e65c2590928c761b7302ad3f37
zzn <hzyhzzn@163.com>
2018-07-23 15:57:47 +0800

    63

---
+++ 63Unique Paths II/63Unique Paths II/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		int m = obstacleGrid.size();
+		int n = obstacleGrid[0].size();
+		vector<vector<int>> dp(m,vector<int> (n,0));
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i==0&&j==0)
+				{
+					if (obstacleGrid[i][j] == 0)
+					{
+						dp[i][j]=1;
+					}
+					else
+					{
+						dp[i][j] = 0;
+					}
+				}
+				else if (i == 0)
+				{
+					if (obstacleGrid[i][j]==0)
+					{
+						dp[i][j] = dp[i][j - 1];
+					}
+					else
+					{
+						dp[i][j] = 0;
+					}
+				}
+				else if(j==0)
+				{
+					if (obstacleGrid[i][j] == 0)
+					{
+						dp[i][j] = dp[i-1][j];
+					}
+					else
+					{
+						dp[i][j] = 0;
+					}
+				}
+				else
+				{
+					if (obstacleGrid[i][j] == 0)
+					{
+						dp[i][j] = dp[i - 1][j]+ dp[i ][j-1];
+					}
+					else
+					{
+						dp[i][j] = 0;
+					}
+				}
+
+			}
+		}
+
+		return dp[m-1][n-1];
+	}
+};
+int main()
+{
+	Solution sol;
+	int res;
+	vector<vector<int>>  ob = {
+		{ 0, 0, 0 },
+		{ 0, 1, 0 },
+		{ 0, 0, 0 }
+	};
+	res = sol.uniquePathsWithObstacles(ob);
+	return 0;
+}
